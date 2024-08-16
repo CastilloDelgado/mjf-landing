700ab0f58583bab5bdc24d6189e754337adba281
Marco Castillo <marcocastillo1997@gmail.com>
2024-08-15 22:40:11 -0600

    Changing background image and adding icons

--- src/views/LinksView.vue
+++ src/views/LinksView.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { ref } from 'vue';
-import { FaFacebook, FaFlag } from "oh-vue-icons/icons"
 import logo from "../assets/images/logo-text-transparent.png"
 
 const links = ref([
@@ -45,31 +44,32 @@ const links = ref([
 </script>
 
 <template>
-    <div class="w-screen h-screen background grid place-content-center pt-12 ">
-        <div class="flex justify-center">
-            <a target="_blank" href="https://mjf-se.com" class="border-2 border-minsk-800 mb-6 bg-white/90 rounded-full w-40 h-40  grid place-content-center hover:scale-105 hover:bg-white transition">
-                <img :src="logo" alt="" class="w-36">
-            </a>
-        </div>
-
-        <div class="p-2 pb-12 space-y-3 no-scrollbar overflow-y-auto w-[80vw] lg:w-[25vw]">
-            <div 
+    <div class="w-screen h-screen module grid place-content-center pt-12 ">
+        <div class="module-inside">
+            <div class="flex justify-center">
+                <a target="_blank" href="https://mjf-se.com" class="border-2 border-minsk-800 mb-6 bg-white/90 rounded-full w-40 h-40  grid place-content-center hover:scale-105 hover:bg-white transition">
+                    <img :src="logo" alt="" class="w-36">
+                </a>
+            </div>
+                
+            <div class="p-2 pb-12 space-y-3 no-scrollbar overflow-y-auto w-[80vw] lg:w-[25vw]">
+                <div 
                 v-for="link in links"
                 :key="link.id"
                 class="group drop-shadow-lg border-2 border-minsk-800 cursor-pointer select-none text-lg w-full bg-minsk-50 px-3 py-2 rounded hover:scale-105 transition hover:bg-minsk-800 hover:text-white"
-            >
-                <a                    
+                >
+                    <a                    
                     :href="link.externalLink"
                     target="_blank"
                     class="flex gap-1"
-                >
+                    >
                     <v-icon v-if="link.icon" :name="link.icon" scale="1.5" class="text-minsk-800 group-hover:text-white"  />
-                    <img  />
-                    <p >{{ link.title }}</p>
+                    <p>{{ link.title }}</p>
                 </a>
             </div>
         </div>
-    </div>
+    </div> 
+</div>
 </template>
 
 <style scoped>
@@ -83,9 +83,24 @@ const links = ref([
 }
 
 .background {
-    background-image: url('../assets/images/2.jpg');
-    filter: brightness(0.80);
-    background-size: cover;
+    filter: brightness(0.90);
+}
+
+.module {
+  position: relative;
+}
+.module::before {
+  content: "";
+  position: absolute;
+  top: 0; left: 0;
+  width: 100%; height: 100%;
+  background-image: url('../assets/images/1.jpg');
+  background-size: cover;
+  filter: brightness(0.6);
+}
+.module-inside {
+  /* This will make it stack on top of the ::before */
+  position: relative;
 }
 
 </style>
